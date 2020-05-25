edc8e61afb433ef4dcb6eb2824ba84c3318a02cb
Yadunand Kamath <yadu10a2000@gmail.com>
2020-05-25 17:50:32 +0530

    Update ML Project.py

--- ML Project.py
+++ ML Project.py
@@ -1,9 +1,9 @@
+# MACHINE LEARNING PROJECT
+
 #!/usr/bin/env python
 # coding: utf-8
 
 # In[5]:
-
-
 import pandas 
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
@@ -25,8 +25,6 @@ from sklearn.ensemble import VotingClassifier
 
 
 # In[7]:
-
-
 # loading the data
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length','sepal-width','petal-length','petal-width','class']
@@ -34,53 +32,39 @@ dataset = read_csv(url,names=names)
 
 
 # In[9]:
-
-
 # dataset
 print(dataset.shape)
 print(dataset.head(20))
 
 
 # In[10]:
-
-
 #statistical summary
 print(dataset.describe())
 
 
 # In[12]:
-
-
 print(dataset.groupby('class').size())
 
 
 # In[13]:
-
-
 # plots
 dataset.plot(kind = 'box',subplots = True,layout = (2,2), sharex = False, sharey = False)
 pyplot.show()
 
 
 # In[14]:
-
-
 #histogram
 dataset.hist()
 pyplot.show()
 
 
 # In[15]:
-
-
 # multivariate plots
 scatter_matrix(dataset)
 pyplot.show()
 
 
 # In[16]:
-
-
 #splitting dataset
 array  = dataset.values
 x = array[:, 0:4]
@@ -89,8 +73,6 @@ X_train,X_validation,Y_train,Y_validation = train_test_split(x , y , test_size =
 
 
 # In[23]:
-
-
 models = []
 models.append(('LR' , LogisticRegression(solver = 'liblinear' , multi_class = 'ovr')))
 models.append(('LDA' , LinearDiscriminantAnalysis()))
@@ -109,8 +91,6 @@ for name,model in models:
 
 
 # In[24]:
-
-
 # compare models
 pyplot.boxplot(results , labels = names)
 pyplot.title('Algorithm Comparison')
@@ -118,8 +98,6 @@ pyplot.show()
 
 
 # In[27]:
-
-
 # predictions on SVM
 model = SVC(gamma = 'auto')
 model.fit(X_train , Y_train)
@@ -127,16 +105,8 @@ predictions = model.predict(X_validation)
 
 
 # In[30]:
-
-
 # evaluate predictions
 print(accuracy_score(Y_validation , predictions))
 print(confusion_matrix(Y_validation , predictions))
 print(classification_report(Y_validation , predictions))
-
-
-# In[ ]:
-
-
-
 
